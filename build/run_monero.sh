ecb1b8673648dab516a48f044aaf10f1f31bcb5f
Kim Ilyong <kimxilxyong@gmail.com>
2018-10-21 09:49:41 +0200

    Initial upload

---
+++ build/run_monero.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# mine is fine
+
+#GPU only miners
+#sudo killall ccminer
+#sleep 5
+# --cuda-launch 16x196 --cuda-bfactor=4 --cuda-bsleep=200
+#cd ~/source/nvidia_miner
+./cn8cardsaver --algo=cn/2 --donate-level=1 -o pool.minexmr.com:5555 -u 422KmQPiuCE7GdaAuvGxyYScin46HgBWMQo4qcRpcY88855aeJrNYWd3ZqE4BKwjhA2BJwQY7T2p6CUmvwvabs8vQqZAzLN.Monero7 -p w=Monero7 --variant 2 --cuda-launch=8x84,8x90 --max-gpu-temp=55 --gpu-temp-falloff=6
+
+
+
+
+
