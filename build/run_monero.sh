9d069b769e27845f072ee82b46b30b3e245dd60a
kimxilxyong <kimxilxyong@gmail.com>
2019-03-23 16:43:55 +0100

    added run_monero.sh

---
+++ build/run_monero.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# mine is fine
+
+#GPU only miners
+#sudo killall ccminer
+#sleep 5 --algo=cn/2 --variant 2
+# --cuda-launch 16x196 --cuda-bfactor=4 --cuda-bsleep=200 
+#cd ~/source/nvidia_miner
+./cn8cardsaver-nvidia -o pool.minexmr.com:5555 -u 422KmQPiuCE7GdaAuvGxyYScin46HgBWMQo4qcRpcY88855aeJrNYWd3ZqE4BKwjhA2BJwQY7T2p6CUmvwvabs8vQqZAzLN.Monero8 -p Monero8 --max-gpu-temp=79 --gpu-temp-falloff=2
+
+
+
+
+
